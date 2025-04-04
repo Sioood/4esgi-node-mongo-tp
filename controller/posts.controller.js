1cf78ba78ff97ab5d5cda5d1d4175a663298e274
Maëlic Pépin <pmaelic@outlook.fr>
2025-04-04 15:48:26 +0200

    Modifier un post

--- controller/posts.controller.js
+++ controller/posts.controller.js
@@ -1,3 +1,6 @@
+const Post = require("./../model/post.model.js");
+const User = require("./../model/user.model.js");
+
 exports.create = async(req, res) =>{
 
 }
@@ -7,7 +10,30 @@ exports.getOne = async(req, res) =>{
 }
 
 exports.editOne = async(req, res) =>{
+    let post = await Post.findOne({_id: req.params.id})
+    let user = await User.findOne({_id: req.token._id})
+    if(!post){
+        return res.status(404).json({errror: "Post intrvable"})
+    }
 
+    if(post.created_by !== user._id){
+        return res.status(404).json({error: "L'utilisateur n'a pas les droits de motifier ce post"})
+    }
+
+    if(req.body.texte){
+        post.texte = req.body.texte;
+    }
+
+    if(req.file){
+        post.picture = './picture' + req.file.filename;
+    }
+
+    try {
+        await post.save();
+        return res.status(201).json(post);
+    }catch(e){
+        return res.status(500).json({error: "Problème lors de la modification d'un post"})
+    }
 }
 
 exports.getAll = async(req, res) =>{
@@ -15,5 +41,5 @@ exports.getAll = async(req, res) =>{
     if(!postList) {
         return res.status(404).json({error : "Aucun post disponible"})
     }
-    return res.status(200).json(userList);
+    return res.status(200).json(postList);
 }
