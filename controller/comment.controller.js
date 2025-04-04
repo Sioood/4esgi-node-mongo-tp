ef9af2eed2f6f4854fca298e38feef77b955be48
Sioood <theodupontpro@gmail.com>
2025-04-04 16:50:46 +0200

    fix: :bug: edit comment autor condition position

--- controller/comment.controller.js
+++ controller/comment.controller.js
@@ -3,49 +3,66 @@ const Post = require("../model/post.model");
 const User = require("../model/user.model");
 
 exports.createComment = async (req, res) => {
-    const postID = req.params.id;
-    const title = req.body.title;
-    const content = req.body.content;
+  const postID = req.params.id;
+  const title = req.body.title;
+  const content = req.body.content;
 
-    if(!postID || !content || !title) return res.status(400).json({message: "Le contenu du commentaire est nécéssaire pour continuer"});
-
-    const post = await Post.findOne({_id: postID});
-    if (!post) return res.status(404).json({message: "Le post est introuvable"});
-
-    try {
-      const newComment = await Comment.create({
-        title: title,
-        content: content,
-        user: req.token._id,
-        post: post.id,
+  if (!postID || !content || !title)
+    return res
+      .status(400)
+      .json({
+        message: "Le contenu du commentaire est nécéssaire pour continuer",
       });
-      return res.status(201).json({
-        message: "Votre commentaire a bien été ajouté",
-        comment: newComment,
-      });
-    } catch (err) {
-      return res
-        .status(500)
-        .json({ message: "Erreur lors de la création du commentaire" });
-    }
 
+  const post = await Post.findOne({ _id: postID });
+  if (!post)
+    return res.status(404).json({ message: "Le post est introuvable" });
+
+  try {
+    const newComment = await Comment.create({
+      title: title,
+      content: content,
+      created_by: req.token._id,
+      post: post.id,
+    });
+    return res.status(201).json({
+      message: "Votre commentaire a bien été ajouté",
+      comment: newComment,
+    });
+  } catch (err) {
+    return res
+      .status(500)
+      .json({ message: "Erreur lors de la création du commentaire" });
+  }
 };
 
 exports.editComment = async (req, res) => {
-    const commentId = req.params.id;
-    const content = req.body.content;
-    let user = await User.findOne({_id: req.token._id})
-    if(comment.created_by !== user._id){
-        return res.status(404).json({error: "L'utilisateur n'a pas les droits de modifier ce commentaire"})
-    }
-    if(!commentId || !content) return res.status(400).json({message: "Le contenu du commentaire est nécéssaire pour continuer"});
+  const commentId = req.params.id;
+  const content = req.body.content;
+  let user = await User.findOne({ _id: req.token._id });
 
-    let comment = await Comment.findOne({_id: commentId});
-    if (!comment) return res.status(404).json({message: "Le commentaire est introuvable"});
+  if (!commentId || !content)
+    return res
+      .status(400)
+      .json({
+        message: "Le contenu du commentaire est nécéssaire pour continuer",
+      });
 
-    comment.content = content;
+  let comment = await Comment.findOne({ _id: commentId });
+  if (comment.created_by !== user._id) {
+    return res.status(404).json({
+      error: "L'utilisateur n'a pas les droits de modifier ce commentaire",
+    });
+  }
 
-    return res.status(201).json({message: "Votre commentaire a bien été modifié"});
+  if (!comment)
+    return res.status(404).json({ message: "Le commentaire est introuvable" });
+
+  comment.content = content;
+
+  return res
+    .status(201)
+    .json({ message: "Votre commentaire a bien été modifié" });
 };
 
 exports.deleteComment = async (req, res) => {
