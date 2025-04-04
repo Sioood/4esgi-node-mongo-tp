9e1bbc3fd1ebf42de4fe3de68234782a256e7f04
Maëlic Pépin <pmaelic@outlook.fr>
2025-04-04 16:36:38 +0200

    Correction syntaxiques

--- controller/posts.controller.js
+++ controller/posts.controller.js
@@ -36,11 +36,11 @@ exports.editOne = async(req, res) =>{
     let post = await Post.findOne({_id: req.params.id})
     let user = await User.findOne({_id: req.token._id})
     if(!post){
-        return res.status(404).json({errror: "Post introuvable"})
+        return res.status(404).json({error: "Post introuvable"})
     }
 
     if(post.created_by !== user._id){
-        return res.status(404).json({error: "L'utilisateur n'a pas les droits de modifier ce post"})
+        return res.status(403).json({error: "L'utilisateur n'a pas les droits de modifier ce post"})
     }
 
     if(req.body.texte){
@@ -48,7 +48,7 @@ exports.editOne = async(req, res) =>{
     }
 
     if(req.file){
-        post.picture = './picture' + req.file.filename;
+        post.picture = './picture/' + req.file.filename;
     }
 
     try {
