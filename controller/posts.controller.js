875502da01ddb4c25eba256c7805922bcd2eefb2
RaphaelAZ <azevedo.raphael2017@gmail.com>
2025-04-04 17:08:26 +0200

    (~) Post fix PUT/DELETE

--- controller/posts.controller.js
+++ controller/posts.controller.js
@@ -25,8 +25,6 @@ exports.create = async(req, res) =>{
     let createpost = await Post.create(post);
 
     res.status(201).json(createpost);
-
-
 }
 
 exports.getOne = async(req, res) =>{
@@ -39,12 +37,11 @@ exports.getOne = async(req, res) =>{
 
 exports.editOne = async(req, res) =>{
     let post = await Post.findOne({_id: req.params.id})
-    let user = await User.findOne({_id: req.token._id})
     if(!post){
         return res.status(404).json({error: "Post introuvable"})
     }
 
-    if(post.created_by !== user._id){
+    if(post.created_by.toString() !== req.token._id){
         return res.status(403).json({error: "L'utilisateur n'a pas les droits de modifier ce post"})
     }
 
@@ -75,7 +72,7 @@ exports.deleteOne = async (req, res) => {
     return res.status(404).json({ error: "Le post n'existe pas" });
   }
     
-  if (post.created_by !== user._id) {
+  if (post.created_by.toString() !== req.token._id) {
     return res.status(404).json({ error: "L'utilisateur n'a pas le droit de supprimer ce post." });
   }
     
