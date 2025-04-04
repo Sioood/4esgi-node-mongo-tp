2785e2f3ae7bba9d03540fdafd57023fe2c8fea4
Sioood <theodupontpro@gmail.com>
2025-04-04 15:47:04 +0200

    feat: :sparkles: add delete one post

--- controller/posts.controller.js
+++ controller/posts.controller.js
@@ -1,19 +1,37 @@
-exports.create = async(req, res) =>{
+const Post = require("./../model/post.model.js");
+const User = require("./../model/user.model.js");
 
-}
+exports.create = async (req, res) => {};
 
-exports.getOne = async(req, res) =>{
+exports.getOne = async (req, res) => {};
 
-}
+exports.editOne = async (req, res) => {};
 
-exports.editOne = async(req, res) =>{
+exports.deleteOne = async (req, res) => {
+  let user = await User.findOne({ _id: req.token._id });
+  if (!user) {
+    return res.status(404).json({ error: "L'utilisateur n'existe pas" });
+  }
 
-}
+  let post = await Post.findOne({ _id: req.params.id });
+  if (!post) {
+    return res.status(404).json({ error: "Le post n'existe pas" });
+  }
+    
+  if (post.created_by !== user._id) {
+    return res.status(404).json({ error: "L'utilisateur n'a pas le droit de supprimer ce post." });
+  }
+    
+    // TODO supprimer tout les commentaires liés au post 
+    
+  await Post.deleteOne({ _id: req.params.id });
+  return res.status(200).json({ message: "Post supprimé" });
+};
 
-exports.getAll = async(req, res) =>{
-    let postList = await Post.find();
-    if(!postList) {
-        return res.status(404).json({error : "Aucun post disponible"})
-    }
-    return res.status(200).json(userList);
-}
+exports.getAll = async (req, res) => {
+  let postList = await Post.find();
+  if (!postList) {
+    return res.status(404).json({ error: "Aucun post disponible" });
+  }
+  return res.status(200).json(userList);
+};
--- routes/post.route.js
+++ routes/post.route.js
@@ -1,10 +1,13 @@
 const express = require("express");
 const router = express.Router();
+const postController = require("./../controller/posts.controller.js");
+
+const authMiddleware = require("../middleware/auth.middleware.js");
 
 router.post('/create', postController.create);
 router.get('/:id', postController.getOne);
-router.put('/:id', postController.editOne);
-router.delete('/:id', postController.deleteOne);
+router.put("/:id", authMiddleware, postController.editOne);
+router.delete('/:id', authMiddleware, postController.deleteOne);
 router.get('/', postController.getAll);
 
 module.exports = router;
