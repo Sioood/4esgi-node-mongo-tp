ffbf126dea2d63fe672fc5bb0b4dd1dec9fef313
RaphaelAZ <azevedo.raphael2017@gmail.com>
2025-04-04 15:37:23 +0200

    (+) Authmiddleware on comment routes

--- routes/comment.route.js
+++ routes/comment.route.js
@@ -1,9 +1,10 @@
 const express = require("express");
 const router = express.Router();
 const commentController = require("../controller/comment.controller");
+const authMiddleware = require("../middleware/auth.middleware.js");
 
-router.post('/create/:id', commentController.createComment);
-router.put('/edit/:id', commentController.editComment);
-router.delete('/delete/:id', commentController.deleteComment);
+router.post('/create/:id', authMiddleware, commentController.createComment);
+router.put('/edit/:id', authMiddleware, commentController.editComment);
+router.delete('/delete/:id', authMiddleware, commentController.deleteComment);
 
 module.exports = router;
