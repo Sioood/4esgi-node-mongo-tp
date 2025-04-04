513f2c745ce78e94aed1c072aba56ac196da6567
RaphaelAZ <azevedo.raphael2017@gmail.com>
2025-04-04 16:18:16 +0200

    (~) Use comment variable

--- controller/comment.controller.js
+++ controller/comment.controller.js
@@ -13,7 +13,7 @@ exports.createComment = async (req, res) => {
     if (!post) return res.status(404).json({message: "Le post est introuvable"});
 
     try {
-        const comment = await Comment.create({
+        const newComment = await Comment.create({
             title: title,
             content: content,
             user: req.token._id,
@@ -22,7 +22,10 @@ exports.createComment = async (req, res) => {
     } catch (err) {
         return res.status(500).json({message: "Erreur lors de la création du commentaire"});
     }
-    return res.status(201).json({message: "Votre commentaire a bien été ajouté"});
+    return res.status(201).json({
+        message: "Votre commentaire a bien été ajouté",
+        comment: newComment
+    });
 };
 
 exports.editComment = async (req, res) => {
