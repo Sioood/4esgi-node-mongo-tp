b5735cc704f57fae05e5e3d14989107a73b1eef3
RaphaelAZ <azevedo.raphael2017@gmail.com>
2025-04-04 14:45:25 +0200

    (+) Auth routes

---
+++ routes/auth.route.js
@@ -0,0 +1,7 @@
+const express = require("express");
+const router = express.Router();
+
+router.post('/register', userController.register);
+router.post('/login', userController.login);
+
+module.exports = router;
