d6fafff4d3169492cb159ea6962c716ca54dbe06
RaphaelAZ <azevedo.raphael2017@gmail.com>
2025-04-04 16:50:06 +0200

    (+) New comment error fix

--- controller/comment.controller.js
+++ controller/comment.controller.js
@@ -1,4 +1,4 @@
-const Comment = require("../model/user.model");
+const Comment = require("../model/comment.model");
 const Post = require("../model/post.model");
 const User = require("../model/user.model");
 
@@ -12,16 +12,14 @@ exports.createComment = async (req, res) => {
     const post = await Post.findOne({_id: postID});
     if (!post) return res.status(404).json({message: "Le post est introuvable"});
 
-    try {
-        const newComment = await Comment.create({
-            title: title,
-            content: content,
-            user: req.token._id,
-            post: post.id
-        });
-    } catch (err) {
-        return res.status(500).json({message: "Erreur lors de la création du commentaire"});
-    }
+    const newComment = await Comment.create({
+        title: title,
+        text: content,
+        user: req.token._id,
+        post: post.id
+    });
+
+    if (!newComment) return res.status(500).json({message: "Une erreur interne est survenue"});
     return res.status(201).json({
         message: "Votre commentaire a bien été ajouté",
         comment: newComment
@@ -31,15 +29,18 @@ exports.createComment = async (req, res) => {
 exports.editComment = async (req, res) => {
     const commentId = req.params.id;
     const content = req.body.content;
+    const title = req.body.title;
+
     let user = await User.findOne({_id: req.token._id})
     if(comment.created_by !== user._id){
         return res.status(404).json({error: "L'utilisateur n'a pas les droits de modifier ce commentaire"})
     }
-    if(!commentId || !content) return res.status(400).json({message: "Le contenu du commentaire est nécéssaire pour continuer"});
+    if(!commentId || !content || !title) return res.status(400).json({message: "Le contenu du commentaire est nécéssaire pour continuer"});
 
     let comment = await Comment.findOne({_id: commentId});
     if (!comment) return res.status(404).json({message: "Le commentaire est introuvable"});
 
+    comment.title = title;
     comment.content = content;
 
     return res.status(201).json({message: "Votre commentaire a bien été modifié"});
